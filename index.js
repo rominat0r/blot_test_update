3eb9b3d0f9052d43ba963da4b00b5abbcdf97132
ruralasket <57713747+ruralasket@users.noreply.github.com>
2019-11-22 20:17:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,88 @@
 window.onload = function(){
-let Inline = Quill.import('blots/inline');
-class BoldBlot extends Inline { }
-BoldBlot.blotName = 'bold';
-BoldBlot.tagName = 'strong';
+let Inline = Quill.import('blots/inline'); 
+
+class BoldBlot extends Inline {}
+    BoldBlot.blotName = 'bold';
+    BoldBlot.tagName = 'strong';
+
+
 class ItalicBlot extends Inline {}
-ItalicBlot.blotName = 'italic';
-ItalicBlot.tagName = 'em';
-class HighlightBlot extends Inline {
-static create() {
-let node = super.create();
-node.setAttribute('style', 'background-color:blue;');
-return node;
+    ItalicBlot.blotName = 'italic';
+    ItalicBlot.tagName = 'em';
+
+
+class HighlightBlot extends Inline 
+{
+    static create() {
+    let node = super.create();
+    node.setAttribute('style', 'background-color:blue;');
+    return node;
 }
 static formats(node) {
-return node.getAttribute('style');
+    return node.getAttribute('style');
 }
 }
+
 HighlightBlot.blotName = 'summary';
 HighlightBlot.tagName = 't-summary';
 Quill.register(BoldBlot);
 Quill.register(ItalicBlot);
 Quill.register(HighlightBlot);
+
 var quill = new Quill('#editor-container');
-quill.keyboard.addBinding({
-key: ' ',
-format:['summary'],
-handler: function(range, context) {
-var text = this.quill.getText(range.index-1,range.index-1);
-if (text.charCodeAt(0) == 32){
-quill.format('summary',false);  
+
+$( "#editor-container" ).click(function() 
+{
+    quill.focus();
+    var myrange = quill.getSelection();
+    var clickformat = quill.getContents(myrange.index,myrange.index);
+    var keyNames;
+    try
+{
+    keyNames = Object.keys(clickformat.ops[0].attributes);
 }
-return true;
+    catch{
+    return true;
+}   
+    if(keyNames[0] == "summary")
+{
+    console.log("%cWorks!", "color: blue; font-size:15px;");
+}   else
+{
+    return true;
+};
+});
+
+quill.keyboard.addBinding
+({
+    key: ' ',
+    format:['summary'],
+    handler: function(range, context) {
+    var text = this.quill.getText(range.index-1,range.index-1);
+    if (text.charCodeAt(0) == 32){
+    quill.format('summary',false);        
+}
+    return true;
 }
 });
+
 $('#bold-button').click(function() {
-quill.format('bold', true);  
-quill.focus(); 
+    quill.format('bold', true);  
+    quill.focus(); 
 });
+
 $('#italic-button').click(function() {
-quill.focus();
-quill.format('italic', true);
+    quill.focus();
+    quill.format('italic', true);
 });
+
 $('#highlight-button').click(function() {
-quill.format('summary',true);   
-quill.focus();    
-console.log("%cWorks!", "color: blue; font-size:15px;");
+    quill.format('summary',true);   
+    quill.focus();   
 });
+
 $('#delta-button').click(function() {
-alert("Open console for delta");
-console.log( quill.getContents());
+    alert("Open console for delta");
+    console.log( quill.getContents());
 });
 }
